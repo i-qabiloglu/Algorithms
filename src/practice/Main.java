7dcf7bf3f57510ed654d3c2b40a498196c2be040
Ibrahim <i.qabilogl@gmail.com>
2022-08-28 12:07:37 +0400

    add bubleSort algorithm

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -3,6 +3,26 @@ package practice;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {56, 23, 42, -6, 32, 15, 69, 3};
+        bubleSort(arr);
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+
+    public static void bubleSort(int... numbers) {
+
+        int temp;
+
+        for (int i = 0; i < numbers.length-1; i++) {
+            for (int j = 0; j < numbers.length - i-1 ; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    temp           = numbers[j + 1];
+                    numbers[j + 1] = numbers[j];
+                    numbers[j]     = temp;
+                }
+            }
+        }
     }
 }
