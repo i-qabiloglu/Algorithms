dace5fa57f7d19b8d108b12392dbf16b181446a3
Ibrahim <i.qabilogl@gmail.com>
2022-09-01 21:05:48 +0400

    add insertion-sort algorithm

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -3,19 +3,75 @@ package algorithms;
 public class Main {
 
     public static void main(String[] args) {
+        int[] arr = {16, 11, 7, 8, 2, 7, 14};
+
+        insertionSort(arr);
+        for (int num : arr) {
+            System.out.println(num);
+        }
 
     }
 
-    public static void bubleSort(int... numbers) {
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] += arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] -= arr[j];
+    }
 
-        for (int i = numbers.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    numbers[j] += numbers[j + 1];
-                    numbers[j + 1] = numbers[j] - numbers[j + 1];
-                    numbers[j] -= numbers[j + 1];
+    //time complexity- O(n^2), space Complexity- O(1)
+    public static void selectionSort(int... numbers) {
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
                 }
             }
+
+            swap(numbers, i, minIndex);
+        }
+    }
+
+    //time complexity- O(n^2)( min- O(n) ), space Complexity- O(1)
+    public static void bubleSort(int... numbers) {
+
+        boolean sorted = false;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            sorted = true;
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    swap(numbers, j, j + 1);
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+    }
+
+    public static void insertionSort2(int... numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (numbers[j] < numbers[j + 1]) {
+                    break;
+                }
+                swap(numbers, j, j + 1);
+            }
+        }
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while(j >= 0 && current < array[j]) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
         }
     }
 
@@ -52,5 +108,7 @@ public class Main {
         }
         return -1;
     }
+
+
 }
 
